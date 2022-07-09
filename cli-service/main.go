2e246022e5564da2215389d68224dbab5da210bc
L3DLP <web@l3dlp.com>
2022-07-09 11:45:26 +0200

    Add CLI version

---
+++ cli-service/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"syscall"
+	"time"
+
+	_ "github.com/qodrorid/godaemon"
+)
+
+type Events struct {
+	Event   chan Event
+	NoEvent chan NoEvent
+}
+
+type Event struct {
+	Name string
+}
+
+type NoEvent struct {
+}
+
+type Error struct {
+	Error error
+}
+
+func atime(fi os.FileInfo) time.Time {
+	return time.Unix(0, fi.Sys().(*syscall.Win32FileAttributeData).LastAccessTime.Nanoseconds())
+}
+
+func main() {
+	args := os.Args
+	var file string
+	if len(args) < 2 {
+		file = "/l3dlp/config"
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		file = args[1]
+	}
+
+	ch := make(chan any)
+	done := make(chan bool)
+	go func() {
+		var latest_updated time.Time
+		var latest_size int64
+		f, err := os.Stat(file)
+		if err != nil {
+			close(ch)
+			log.Println(err)
+			return
+
+		}
+
+		atime1 := atime(f)
+
+		latest_updated = atime1
+		latest_size = f.Size()
+		for {
+
+			f_n, err := os.Stat(file)
+			if err != nil {
+				ch <- Error{
+					Error: err,
+				}
+				continue
+			}
+
+			atime := atime(f_n)
+			if atime.Format("2006-01-02 15:04:05") != latest_updated.Format("2006-01-02 15:04:05") {
+				latest_updated = atime
+				if latest_size == f_n.Size() {
+					ch <- Event{
+						Name: f.Name() + ": OPENED",
+					}
+					continue
+				} else if latest_size != f_n.Size() {
+					latest_size = f_n.Size()
+					ch <- Event{
+						Name: f.Name() + ": WRITE",
+					}
+					continue
+				} else {
+					ch <- NoEvent{}
+					continue
+				}
+			} else {
+				ch <- NoEvent{}
+				continue
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case evt, ok := <-ch:
+				_ = ok
+				switch evt.(type) {
+				case Event:
+					err := fmt.Println(evt.(Event).Name)
+					if err != nil {
+						log.Println(err)
+					}
+					continue
+				case Error:
+					/*
+						err := beeep.Alert("Spyfile", evt.(Error).Error.Error(), "logo.jpg")
+						if err != nil {
+							log.Println(err)
+						}
+					*/
+					continue
+				}
+			}
+		}
+	}()
+
+	<-done
+}
---
+++ windows/go.mod
@@ -0,0 +1,19 @@
+module event
+
+go 1.18
+
+require (
+	github.com/gen2brain/beeep v0.0.0-20220322123227-629384b5779c
+	github.com/qodrorid/godaemon v0.0.0-20170331035433-aeac1c057d11
+)
+
+require (
+	github.com/gen2brain/dlgs v0.0.0-20211108104213-bade24837f0b // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20220221023154-0b2280d3ff96 // indirect
+	github.com/gopherjs/gopherwasm v1.1.0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
+)
